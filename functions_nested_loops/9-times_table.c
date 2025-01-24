261878508d240c2aa26ff028c95db9d78007d313
cyusakeny <gasananike@gmail.com>
2025-01-24 18:16:03 +0200

    task 10

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * times_table - Prints multiplication table
+ *
+ * Description: This function prints the multiplication table
+ */
+
+void times_table(void)
+{
+
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+printf("%d", (j * i));
+if (j != 9)
+{
+printf(", ");
+}
+
+}
+printf("\n");
+}
+}
