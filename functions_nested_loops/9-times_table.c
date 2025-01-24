3d24dde7b56b88bccbf2c003be54d9a9cd74949f
cyusakeny <gasananike@gmail.com>
2025-01-24 18:30:29 +0200

    task 10 fix 3

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,31 +9,44 @@ void times_table(void)
 {
 
 int i, j;
-char digit1, digit2;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-int num = j * i;
-if (num < 10)
-{
- _putchar(' ');   
-_putchar(num + '0');
-}
-else
-{
-digit1 = (num / 10) + '0';
-digit2 = (num % 10) + '0';
-_putchar(digit1);
-_putchar(digit2);
-}
-if (j != 9)
-{
-_putchar(',');
-_putchar(' ');
-}
+    char digit1, digit2;
 
-}
-_putchar('\n');
-}
+    for (i = 0; i < 10; i++)  
+    {
+        for (j = 0; j < 10; j++) 
+        {
+            int product = i * j;
+
+            
+            if (product < 10)
+            {
+                if (j != 0) 
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                _putchar(product + '0');
+            }
+            else  
+            {
+                digit1 = (product / 10) + '0'; 
+                digit2 = (product % 10) + '0'; 
+
+                if (j != 0) 
+                {
+                    _putchar(' ');
+                }
+                _putchar(digit1);
+                _putchar(digit2);
+            }
+
+           
+            if (j != 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
