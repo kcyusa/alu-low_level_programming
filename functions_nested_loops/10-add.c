71268464422bee54f7f8d5807469c3da4adca7da
cyusakeny <gasananike@gmail.com>
2025-01-24 19:58:28 +0200

    task 11

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+* add - adds two numbers
+* @a: first number
+* @b: second number
+* Description: This function adds two numbers
+* Return: a+b
+*/
+
+int add(int a, int b)
+{
+
+return (a + b);
+}
