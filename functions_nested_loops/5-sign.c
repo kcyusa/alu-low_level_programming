58bf2717840573eae60d87cb149f2f04fef3a3ec
cyusakeny <gasananike@gmail.com>
2025-01-24 16:35:34 +0200

    task 6 v1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -6,7 +6,8 @@
  *
  * Return: 1  , 0 or -1
  */
-int print_sign(int n){
+int print_sign(int n)
+{
 if (n > 0)
 {
 _putchar('+');
@@ -15,11 +16,11 @@ return (1);
 else if (n == 0)
 {
 _putchar('0');
-return (0);    
+return (0);
 }
 else
 {
 _putchar('-');
-return (-1);     
+return (-1);
 }
 }
