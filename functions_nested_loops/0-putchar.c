1e228e87ee84b16fb00f4606c8261fe029f42f61
cyusakeny <gasananike@gmail.com>
2025-01-24 14:15:48 +0200

    task 1 fix 4

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * my_function - Writes a single character to the standard output
+ * @c: The character to be written
+ *
+ * Description: This function takes a single character as input
+ * and writes it to the standard output using the write system call.
+ * It is a low-level implementation that bypasses higher-level
+ * buffering.
+ *
+ * Return: None
+ */
+void my_function(char c)
+{
+write(1, &c, 1);
+}
+
 /**
  * main - Entry point of the program
  *
