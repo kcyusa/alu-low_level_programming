8fe38baf88d67f4844cb3d43bec510bfdc4705de
cyusakeny <gasananike@gmail.com>
2025-02-07 21:34:20 +0200

    task 2

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swap two numbers
+ *
+ * @a: pointer to check
+ * @b: pointer to check
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int x = *a;
+*a = *b;
+*b = x;
+}
