6796000ec33a06092af51d06af21a3bfefe8a8b1
cyusakeny <gasananike@gmail.com>
2025-02-07 23:03:56 +0200

    task 10

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+/**
+ * _strcpy -  function to copy from one string to another
+ *
+ * @dest: pointer to destination
+ * @src: pointer to src
+ * Return: pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *ptr = dest;
+while ((*dest++ = *src++))
+return (ptr);
+}
