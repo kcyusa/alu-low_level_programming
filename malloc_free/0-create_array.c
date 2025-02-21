989fb52a3a8322e6054b25cc3ed88580cb535545
cyusakeny <gasananike@gmail.com>
2025-02-21 18:58:30 +0200

    fix task 1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
+
+/**
+ * create_array - assigns the arr
+ * @size: size of array
+ * @c: initial character
+ * Return: Always array.
+ */
 char *create_array(unsigned int size, char c)
 {
 char *arr;
