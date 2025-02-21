030ab3bc71a2056d45be589cfe64e89c5181928b
cyusakeny <gasananike@gmail.com>
2025-02-21 19:07:29 +0200

    task 2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strdup - duplicate
+ *@str: string to duplicate
+ * Return: duplicate string.
+ */
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int len, i;
+if (str == NULL)
+{
+return (NULL);
+}
+len = strlen(str);
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+{
+return (NULL);
+}
+for (i = 0; i <= len; i++)
+{
+dup[i] = str[i];
+}
+
+return (dup);
+}
