86983e16db6c524d4e8697769697c788c980bf47
cyusakeny <gasananike@gmail.com>
2025-03-01 18:27:41 +0200

    task 6

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -86,18 +86,3 @@ d->age = age;
 d->owner = owner_copy;
 return (d);
 }
-
-
-/**
- * free_dog - Frees memory allocated for a dog.
- * @d: Pointer to the dog to be freed.
- */
-void free_dog(dog_t *d)
-{
-if (d != NULL)
-{
-free(d->name);
-free(d->owner);
-free(d);
-}
-}
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Frees memory allocated for a dog.
+ * @d: Pointer to the dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
