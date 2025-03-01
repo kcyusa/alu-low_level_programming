7616cd02a949050743b4d874c503dc63e36c6d9d
cyusakeny <gasananike@gmail.com>
2025-03-01 17:28:38 +0200

    task 5

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - Creates a new dog with a copy of name and owner.
+ * @name: Pointer to the dog's name.
+ * @age: The dog's age.
+ * @owner: Pointer to the owner's name.
+ *
+ * Return: Pointer to the new dog, or NULL if it fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+char *name_copy, *owner_copy;
+
+if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+return (NULL);
+
+name_copy = malloc(strlen(name) + 1);
+if (name_copy == NULL)
+{
+free(d);
+return (NULL);
+}
+strcpy(name_copy, name);
+
+owner_copy = malloc(strlen(owner) + 1);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(d);
+return (NULL);
+}
+strcpy(owner_copy, owner);
+
+d->name = name_copy;
+d->age = age;
+d->owner = owner_copy;
+return (d);
+}
+
+/**
+ * free_dog - Frees memory allocated for a dog.
+ * @d: Pointer to the dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
