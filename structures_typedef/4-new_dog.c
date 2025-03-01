f0077b7f19f44b65aa88599c3fc4183d28853650
cyusakeny <gasananike@gmail.com>
2025-03-01 18:21:23 +0200

    task 5 fix 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,6 +19,32 @@ return (ptr - str);
 }
 
 /**
+ * _strcpy - copy from src to dest
+ * @src: Pointer to the source.
+ * @dest: Pointer to the destination.
+ * Return: new destination.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+if (src == NULL || dest == NULL)
+{
+return NULL;
+}
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
+
+/**
  * new_dog - Creates a new dog with a copy of name and owner.
  * @name: Pointer to the dog's name.
  * @age: The dog's age.
@@ -45,7 +71,7 @@ if (name_copy == NULL)
 free(d);
 return (NULL);
 }
-strcpy(name_copy, name);
+_strcpy(name_copy, name);
 owner_copy = malloc(_strlen(owner) + 1);
 if (owner_copy == NULL)
 {
@@ -53,7 +79,7 @@ free(name_copy);
 free(d);
 return (NULL);
 }
-strcpy(owner_copy, owner);
+_strcpy(owner_copy, owner);
 
 d->name = name_copy;
 d->age = age;
