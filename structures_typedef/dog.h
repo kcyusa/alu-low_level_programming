33df8ed7d70dd3d5dc3db67ff2e629c775691887
cyusakeny <gasananike@gmail.com>
2025-03-01 16:48:05 +0200

    task 1 fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Defines a dog's attributes.
+ * @name: Pointer to the dog's name (string).
+ * @age: The dog's age (float).
+ * @owner: Pointer to the owner's name (string).
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
+#endif
